120e13ae0be036d6300b60b2d39be45102117ba3
KorotkovPavel <162363366+KorotkovPavel@users.noreply.github.com>
2024-09-25 14:02:03 +0300

    Add files via upload

---
+++ PJ-04_m30_rostelecom/config.py
@@ -0,0 +1,19 @@
+class RostelecomInfo():
+    URL_START = 'https://start.rt.ru/'
+    URL_ELK = 'https://lk.rt.ru/'
+    URL_Key = 'https://key.rt.ru/'
+    URL_Online = 'https://my.rt.ru/'
+    URL_SmartHome = 'https://lk.smarthome.rt.ru/'
+
+    login = "валидный логин"
+    invalid_login = "не валидный логин"
+    mail = "rewez90@yandex.ru"
+
+
+    valid_password1 = "Rostelecom90"
+    password2 = "пароль009"
+
+    tab_phone_text = "89040360738"
+    tab_mail_text = "rewez90@yandex.ru"
+    tab_login_text = "логин"
+    tab_personal_account_text = "лицевой счёт"
